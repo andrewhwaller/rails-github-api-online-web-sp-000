aa0d1a82a33df42969585c2dabaa19634407af06
Andrew Waller <andrewhwaller@gmail.com>
2019-09-18 23:56:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    client_id = ENV['GITHUB_CLIENT']
     redirect_uri = CGI.escape("http://localhost:3000/auth")
   end
 
